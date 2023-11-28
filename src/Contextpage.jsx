48b552185c6a51af2c98fe88a79bee678e0f470e
TranquyenVN <tranquyenqtkdtm.bmt@gmail.com>
2023-11-28 18:34:28 +0700

    change region

--- src/Contextpage.jsx
+++ src/Contextpage.jsx
@@ -1,109 +1,107 @@
-import { createContext, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-//=== google firebase import start ===
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { auth } from '../firebase';
-import { useAuthState } from "react-firebase-hooks/auth"
-// ===================================
-import { toast } from 'react-toastify';
+/** @format */
 
-const Contextpage = createContext();
+import { createContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+//=== google firebase import start ===
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../firebase'
+// ===================================
+import { toast } from 'react-toastify'
+
+const Contextpage = createContext()
 
 export function MovieProvider({ children }) {
-
-  const [header, setHeader] = useState("Trending");
+  const [header, setHeader] = useState('Trending')
   const [totalPage, setTotalPage] = useState(null)
-  const [movies, setMovies] = useState([]);
-  const [searchedMovies, setSearchedMovies] = useState([]);
-  const [trending, setTrending] = useState([]);
-  const [upcoming, setUpcoming] = useState([]);
-  const [page, setPage] = useState(1);
-  const [activegenre, setActiveGenre] = useState(28);
+  const [movies, setMovies] = useState([])
+  const [searchedMovies, setSearchedMovies] = useState([])
+  const [trending, setTrending] = useState([])
+  const [upcoming, setUpcoming] = useState([])
+  const [page, setPage] = useState(1)
+  const [activegenre, setActiveGenre] = useState(28)
   const [genres, setGenres] = useState([])
-  const [loader, setLoader] = useState(true);
-  const [backgenre, setBackGenre] = useState(false);
-  const [user, setUser] = useAuthState(auth)//=======> firebase custom hooks state
-  const navigate = useNavigate();// =====> navigate page
+  const [loader, setLoader] = useState(true)
+  const [backgenre, setBackGenre] = useState(false)
+  const [user, setUser] = useAuthState(auth) //=======> firebase custom hooks state
+  const navigate = useNavigate() // =====> navigate page
 
-  const APIKEY = import.meta.env.VITE_API_KEY;
-
+  const APIKEY = import.meta.env.VITE_API_KEY
 
   useEffect(() => {
     if (page < 1) {
-      setPage(1)  // Increment page to 1 if it is less than 1.
+      setPage(1) // Increment page to 1 if it is less than 1.
     }
-  }, [page]);
-
+  }, [page])
 
   const filteredGenre = async () => {
     const data = await fetch(
-      `https://api.themoviedb.org/3/discover/movie?with_genres=${activegenre}&api_key=${APIKEY}&with_origin_country=IN&page=${page}`
-    );
-    const filteredGenre = await data.json();
-    setMovies(movies.concat(filteredGenre.results)); // Concat new movies with previous movies, on genre change movies are reset to [] so that only movies of new genre will appear, check out useEffect on top for more information.
-    setTotalPage(filteredGenre.total_pages);
-    setLoader(false);
-    setHeader("Genres");
-  };
+      `https://api.themoviedb.org/3/discover/movie?with_genres=${activegenre}&api_key=${APIKEY}&with_origin_country=VN&page=${page}`,
+    )
+    const filteredGenre = await data.json()
+    setMovies(movies.concat(filteredGenre.results)) // Concat new movies with previous movies, on genre change movies are reset to [] so that only movies of new genre will appear, check out useEffect on top for more information.
+    setTotalPage(filteredGenre.total_pages)
+    setLoader(false)
+    setHeader('Genres')
+  }
 
   const fetchSearch = async (query) => {
     const data = await fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&with_origin_country=IN&language=en-US&query=${query}&page=1&include_adult=false`
-    );
-    const searchmovies = await data.json();
-    setSearchedMovies(searchmovies.results); 
-    setLoader(false);
-    setHeader(`Results for "${query}"`);
+      `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&with_origin_country=VN&language=vi-VN&query=${query}&page=1&include_adult=false`,
+    )
+    const searchmovies = await data.json()
+    setSearchedMovies(searchmovies.results)
+    setLoader(false)
+    setHeader(`Results for "${query}"`)
   }
 
   const fetchGenre = async () => {
     const data = await fetch(
-      `https://api.themoviedb.org/3/genre/movie/list?api_key=${APIKEY}&with_origin_country=IN&language=en-US`
-    );
-    const gen = await data.json();
-    setGenres(gen.genres);
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${APIKEY}&with_origin_country=VN&language=vi-VN`,
+    )
+    const gen = await data.json()
+    setGenres(gen.genres)
   }
 
   const fetchTrending = async () => {
     const data = await fetch(
-      `https://api.themoviedb.org/3/trending/movie/day?api_key=${APIKEY}&with_origin_country=IN&page=${page}`
-    );
-    const trend = await data.json();
-    setTrending(trending.concat(trend.results));
-    setTotalPage(trend.total_pages);
-    setLoader(false);
-    setHeader("Trending Movies");
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${APIKEY}&with_origin_country=VN&page=${page}`,
+    )
+    const trend = await data.json()
+    setTrending(trending.concat(trend.results))
+    setTotalPage(trend.total_pages)
+    setLoader(false)
+    setHeader('Trending Movies')
   }
 
   const fetchUpcoming = async () => {
     const data = await fetch(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=${APIKEY}&with_origin_country=IN&language=en-US&page=${page}`
-    );
-    const upc = await data.json();
-    setUpcoming(upcoming.concat(upc.results));
-    setTotalPage(upc.total_pages);
-    setLoader(false);
-    setHeader("Upcoming Movies");
+      `https://api.themoviedb.org/3/movie/upcoming?api_key=${APIKEY}&with_origin_country=VN&language=vi-VN&page=${page}`,
+    )
+    const upc = await data.json()
+    setUpcoming(upcoming.concat(upc.results))
+    setTotalPage(upc.total_pages)
+    setLoader(false)
+    setHeader('Upcoming Movies')
   }
 
   // creat local storage
   const GetFavorite = () => {
-    setLoader(false);
-    setHeader("Favorite Movies");
+    setLoader(false)
+    setHeader('Favorite Movies')
   }
 
-
   //<========= firebase Google Authentication ========>
-  const googleProvider = new GoogleAuthProvider();// =====> google auth provide
+  const googleProvider = new GoogleAuthProvider() // =====> google auth provide
 
   const GoogleLogin = async () => {
     try {
-      const result = await signInWithPopup(auth, googleProvider);
-      navigate("/")
-      toast.success("Login successfully");
+      const result = await signInWithPopup(auth, googleProvider)
+      navigate('/')
+      toast.success('Login successfully')
     } catch (err) {
       console.log(err)
-      navigate("/")
+      navigate('/')
     }
   }
   // <==========================================================>
@@ -136,13 +134,12 @@ export function MovieProvider({ children }) {
         totalPage,
         searchedMovies,
         GoogleLogin,
-        user
+        user,
       }}
     >
       {children}
     </Contextpage.Provider>
-  );
-
+  )
 }
 
 export default Contextpage
